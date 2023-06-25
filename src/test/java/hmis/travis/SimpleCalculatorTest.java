1ecf70dfe5c4b59f369f39002c9f64d0708ffad3
ALEJANDRO TOMAS <asm978@inlumine.ual.es>
2023-06-25 18:54:41 +0200

    subida contenido travis

---
+++ src/test/java/hmis/travis/SimpleCalculatorTest.java
@@ -0,0 +1,27 @@
+package hmis.travis;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SimpleCalculatorTest {
+	@Test
+	public void testAdd() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.add(1, 1), 2);
+	}
+	@Test
+	public void testSub() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.sub(2, 1), 1);
+	}
+	@Test
+	public void testMul() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.mul(2, 5), 10);
+	}
+	@Test
+	public void testDiv() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.div(8, 2), 4);
+	}
+}
